5b9d60fcd0c7e0ab1d76af885abc7891a60ee40e
Victor Letunovsky <victor.letunovsky@gmail.com>
2019-07-03 23:54:33 +0300

    [T06.01-Exercise-SetupTheActivity]
    1. Create a new Activity called SettingsActivity
    2. Create a new resource folder called menu
    3. Create visualizer_menu.xml in menu and add a menu item in it
    4. Add the menu to the menu bar
    5. Handle "Settings" menu item click to open SettingsActivity
    6. Make sure activity_settings.xml is in RelativeLayout

---
+++ Lesson06-Visualizer-Preferences/T06.01-Exercise-SetupTheActivity/app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,35 @@
+package android.example.com.visualizerpreferences;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    // COMPLETED (1) Create a new Empty Activity named SettingsActivity; make sure to generate the
+    // activity_settings.xml layout file as well and add the activity to the manifest
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+        ActionBar actionBar = this.getSupportActionBar();
+
+        // Set the action bar back button to look like an up button
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        // When the home button is pressed, take the user back to the VisualizerActivity
+        if (id == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
