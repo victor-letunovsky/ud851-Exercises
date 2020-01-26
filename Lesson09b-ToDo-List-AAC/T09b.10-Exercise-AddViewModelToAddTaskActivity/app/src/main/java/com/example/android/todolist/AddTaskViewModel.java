6637cdf97c8596ac1cff3c9569fafb91066ba119
Victor Letunovsky <victor.letunovsky@gmail.com>
2020-01-26 23:58:06 +0200

    [T09b.10-Exercise-AddViewModelToAddTaskActivity]

--- Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModel.java
+++ Lesson09b-ToDo-List-AAC/T09b.10-Exercise-AddViewModelToAddTaskActivity/app/src/main/java/com/example/android/todolist/AddTaskViewModel.java
@@ -1,12 +1,29 @@
 package com.example.android.todolist;
 
-// TODO (5) Make this class extend ViewModel
-public class AddTaskViewModel {
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.ViewModel;
+import android.util.Log;
 
-    // TODO (6) Add a task member variable for the TaskEntry object wrapped in a LiveData
+import com.example.android.todolist.database.AppDatabase;
+import com.example.android.todolist.database.TaskEntry;
 
-    // TODO (8) Create a constructor where you call loadTaskById of the taskDao to initialize the tasks variable
+// COMPLETED (5) Make this class extend ViewModel
+public class AddTaskViewModel extends ViewModel {
+
+    private static final String TAG = AddTaskViewModel.class.getSimpleName();
+
+    // COMPLETED (6) Add a task member variable for the TaskEntry object wrapped in a LiveData
+    private LiveData<TaskEntry> task;
+
+    // COMPLETED (8) Create a constructor where you call loadTaskById of the taskDao to initialize the task variable
     // Note: The constructor should receive the database and the taskId
+    public AddTaskViewModel(AppDatabase database, int taskId) {
+        Log.d(TAG, "Actively retrieving a specific task from the DataBase");
+        this.task = database.taskDao().loadTaskById(taskId);
+    }
 
-    // TODO (7) Create a getter for the task variable
+    // COMPLETED (7) Create a getter for the task variable
+    public LiveData<TaskEntry> getTask() {
+        return task;
+    }
 }
